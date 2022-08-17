a5487c51f0eef616b67b4eec14b3a7ca07e3aeeb
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2022-08-17 19:13:29 -0300

    added MapViewModel

--- app/src/main/java/com/uade/dist/morfando/ui/view/MapActivity.kt
+++ app/src/main/java/com/uade/dist/morfando/ui/view/MapActivity.kt
@@ -1,18 +1,19 @@
 package com.uade.dist.morfando.ui.view
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.uade.dist.morfando.R
+import com.uade.dist.morfando.ui.viewmodel.MapViewModel
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var map: GoogleMap
+    private val mapViewModel: MapViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,16 +26,20 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    override fun onMapReady(googmeMap: GoogleMap) {
-        map = googmeMap
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        mapViewModel.onMapReady()
 
-        val coordinates = LatLng(28.043893, -16.539329)
-        val marker = MarkerOptions().position(coordinates).title("Tenerife")
-        map.addMarker(marker)
-        map.animateCamera(
-            CameraUpdateFactory.newLatLngZoom(coordinates, 18f),
-            4000,
-            null
-        )
+        mapViewModel.marker.observe(this) {
+            map.addMarker(it)
+        }
+
+        mapViewModel.coordinatesZoom.observe(this) {
+            map.animateCamera(
+                CameraUpdateFactory.newLatLngZoom(it, 18f),
+                4000,
+                null
+            )
+        }
     }
 }
