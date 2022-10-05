e99121335bb2ef0f3717ae52f8dd3e119082b28f
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2022-10-05 20:53:32 -0300

    added google sign in

--- app/src/main/java/com/uade/dist/morfando/ui/view/LoginActivity.kt
+++ app/src/main/java/com/uade/dist/morfando/ui/view/LoginActivity.kt
@@ -2,8 +2,16 @@ package com.uade.dist.morfando.ui.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.uade.dist.morfando.R
 import com.uade.dist.morfando.data.local.SHARED_PREFERENCES_NAME
 import com.uade.dist.morfando.data.local.SHARED_PREFERENCES_TOKEN
@@ -16,6 +24,8 @@ import kotlinx.coroutines.launch
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private val homeViewModel: LoginViewModel by viewModels()
+    private lateinit var googleSignInOptions: GoogleSignInOptions
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Morfando)
@@ -39,6 +49,52 @@ class LoginActivity : AppCompatActivity() {
                 startActivity(Intent(this@LoginActivity, HomeActivity::class.java))
                 finish()
             }
+        }
+
+
+        googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            //TODO .requestIdToken(getString(R.string.server_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+
+        binding.loginGoogle.setOnClickListener {
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        val intent = googleSignInClient.signInIntent
+        this.startActivityForResult(intent, 1000)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1000) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val result = task.getResult(ApiException::class.java)
+
+                loginSuccess(result)
+            } catch (e: Exception) {
+                Toast.makeText(this, "Algo salió mal, vuelve a intentarlo en unos instantes", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun loginSuccess(account: GoogleSignInAccount?) {
+        account?.run {
+            Log.d("account- displayName", this.displayName!!)
+            Log.d("account- familyName", this.familyName!!)
+            Log.d("account- givenName", this.givenName!!)
+            Log.d("account- email", this.email!!)
+            Log.d("account- id", this.id!!)
+            Log.d("account- id", this.idToken!!) // TODO enviar id a backend y validarlo https://developers.google.com/identity/sign-in/android/backend-auth
+
+            val sharedPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
+            sharedPreferences.edit().putString(SHARED_PREFERENCES_TOKEN, "1234").apply() //FIXME enviar idToken
+            startActivity(Intent(this@LoginActivity, HomeActivity::class.java))
+            finish()
         }
     }
 }
