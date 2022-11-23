f841002359a0b10c634166a1a2b2ecbf0471a206
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2022-11-23 19:53:16 -0300

    removed hardcoding result

--- app/src/main/java/com/uade/dist/morfando/ui/viewmodel/RestaurantDetailsViewModel.kt
+++ app/src/main/java/com/uade/dist/morfando/ui/viewmodel/RestaurantDetailsViewModel.kt
@@ -31,46 +31,7 @@ class RestaurantDetailsViewModel: ViewModel() {
                     requestState.value = RequestState.SUCCESS
                 }
                 .onFailure {
-                    // TODO dejar esto: requestState.value = RequestState.FAILURE(it.toString())
-
-                    // TODO eliminar:
-                    restaurantDetails.value = RestaurantDetailsModel(
-                        OpenHoursModel(
-                            OpenHoursDayModel("Lunes", "08:00", "10:00", true),
-                            OpenHoursDayModel("Martes", "08:00", "10:00", true),
-                            OpenHoursDayModel("Miercoles", "08:00", "10:00", true),
-                            OpenHoursDayModel("Jueves", "08:00", "10:00", true),
-                            OpenHoursDayModel("Viernes", "08:00", "10:00", true),
-                            OpenHoursDayModel("Sábado", null, null, false),
-                            OpenHoursDayModel("Domingo", "08:00", "12:00", true)
-                        ),
-                        "Lorem ipsum dolor set amet",
-                        listOf(
-                            RatingModel("code",  3.0.toLong(), "Copado", "Lorem ipsum dolor set amet", "https://i.imgur.com/GB7lTPH.jpeg"),
-                            RatingModel("code",  2.0.toLong(), "Horrible", "Lorem ipsum dolor set amet", "https://i.imgur.com/OK1u0FO.jpeg")
-                        ),
-                        listOf(
-                            "https://i.imgur.com/GB7lTPH.jpeg",
-                            "https://i.imgur.com/GB7lTPH.jpeg",
-                            "https://i.imgur.com/GB7lTPH.jpeg",
-                            "https://i.imgur.com/GB7lTPH.jpeg",
-                            "https://i.imgur.com/GB7lTPH.jpeg",
-                            "https://i.imgur.com/GB7lTPH.jpeg",
-                            "https://i.imgur.com/GB7lTPH.jpeg",
-                            "https://i.imgur.com/GB7lTPH.jpeg",
-                            "https://i.imgur.com/GB7lTPH.jpeg",
-                            "https://i.imgur.com/GB7lTPH.jpeg",
-                            "https://i.imgur.com/GB7lTPH.jpeg",
-                        ),
-                        false,
-                        "calle",
-                        "123",
-                        "Buenos aires",
-                        "avellaneda",
-                        "argentina"
-                    )
-                    ratingsList.postValue(restaurantDetails.value!!.ratings!!)
-                    requestState.value = RequestState.SUCCESS
+                    requestState.value = RequestState.FAILURE(it.toString())
                 }
         }
     }
